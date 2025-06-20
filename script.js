3d4a88117fb0d024e8b0ef6737e191ddab4b9fb9
salommenbulbosit <abdulbositmirgani@gmail.com>
2025-06-20 18:16:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let userName= prompt('ismingizni kiriting')
+console.log(userName);
+
+let userSurname = prompt('familiyangizni kiriting')
+console.log(userSurname);
+let userAge = prompt('yoshingizm kiriting')
+console.log(userAge);
+let userLove = prompt('yoqtirgan faningizni kiriting')
+console.log(userLove);
+let userLocation = prompt('yashaydigan tumaningiz va shaxaringizni kiriting')
+console.log(userLocation);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
